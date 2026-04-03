efe97321a524f2960ff94dc22df59df74ff1f64c
Admin <duytinh.vn@gmail.com>
2026-04-03 19:13:40 +0700

    bt

--- src/practicalexam/Main.java
+++ src/practicalexam/Main.java
@@ -7,8 +7,8 @@ public class Main {
         try {
             System.out.println("BẮT ĐẦU CHẠY CHƯƠNG TRÌNH...\n");
 
-            // --- TEST CÂU 1 ---
-            System.out.println("[TEST CÂU 1: Insert dữ liệu]");
+
+            System.out.println("Insert dữ liệu");
             dao.insertNational("Vietnam");
             dao.insertNational("USA");
             dao.insertNational("Japan");
@@ -20,15 +20,15 @@ public class Main {
             dao.insertPlayer(1, "Tjpro", 9999, 50);
 
             // --- TEST CÂU 2 ---
-            System.out.println("\n[TEST CÂU 2: Hiển thị tất cả]");
+            System.out.println("\n Hiển thị tất cả");
             dao.displayAll();
 
             // --- TEST CÂU 3 ---
-            System.out.println("[TEST CÂU 3: Tìm kiếm theo tên]");
+            System.out.println("Tìm kiếm theo tên");
             dao.displayAllByPlayerName("Player 2");
 
             // --- TEST CÂU 4 ---
-            System.out.println("\n[TEST CÂU 4: Hiển thị Top 10]");
+            System.out.println("\n Hiển thị Top 10");
             dao.displayTop10();
 
         } catch (Exception e) {
